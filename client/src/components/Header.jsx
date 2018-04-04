85b2e7d7c2fba7061ba96bd4a291228a4ea27d94
Daniel Sanchez <atlow.archive@gmail.com>
2018-04-04 16:58:51 -0700

    [animations] setup provisional animations

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,10 +10,10 @@ class Header extends React.Component {
     return (
       <div className="jumbotron jumbotron-fluid jumbotron-custom">
         <div className="jumbotron-text container">
-          <h1 className="display-4">Hello, I'm Daniel</h1>
-          <p className="lead">Fullstack Web Developer & UI / UX Enthusiast</p>
+          <h1 className="display-4 animated fadeInUp">Hello, I'm Daniel</h1>
+          <p className="lead animated fadeInUp">Fullstack Web Developer & UI / UX Enthusiast</p>
         </div>
-        <div className="container goDown">
+        <div className="container goDown animated fadeInUp">
           <a href="#about">
             <span className="far fa-arrow-alt-circle-down fa-3x" color="#FFF"></span>
           </a>
